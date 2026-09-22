505820aa98aa76f465096ba3442fd8968a7aa08f
ikae <ikaesujeel@gmail.com>
2026-09-22 10:18:58 +0900

    test: DB exception wrapper differences

--- backend/src/test/java/com/ikae/snowthing/domain/post/concurrency/ReactionConcurrencyIntegrationTest.java
+++ backend/src/test/java/com/ikae/snowthing/domain/post/concurrency/ReactionConcurrencyIntegrationTest.java
@@ -23,7 +23,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -319,7 +318,7 @@ class ReactionConcurrencyIntegrationTest {
                                         users.getFirst(),
                                         DEFAULT_CLIENT_IP,
                                         null))
-                .isInstanceOf(JpaSystemException.class);
+                .isInstanceOf(DataAccessException.class);
 
         assertInvariant(EXPECTED_NO_REACTION);
     }
@@ -339,7 +338,7 @@ class ReactionConcurrencyIntegrationTest {
                                         users.getFirst(),
                                         DEFAULT_CLIENT_IP,
                                         null))
-                .isInstanceOf(JpaSystemException.class);
+                .isInstanceOf(DataAccessException.class);
 
         assertInvariant(EXPECTED_SINGLE_REACTION);
     }
